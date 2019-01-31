07c86650024d66f1a6adbeb2bb52176e214acf7f
shvetsovdmitry <dmitryshvetsov1996@gmail.com>
2019-01-31 09:38:22 +0500

    Added some new functional.

--- backend.py
+++ backend.py
@@ -1,11 +1,14 @@
 import mysql.connector
 from mysql.connector import errorcode
+import csv
+import re
 
 
 """Connecting to the database."""
 try:
-    conn = mysql.connector.connect(user = 'root', host = '127.0.0.1', database = 'backend', unix_socket = '/var/run/mysqld/mysql.sock')
+    conn = mysql.connector.connect(user = 'dmitry', host = '127.0.0.1', database = 'backend', unix_socket = '/var/run/mysqld/mysqld.sock')
     cursor = conn.cursor()
+# FIX THIS SHIT
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print('Something is wrong with your user name or password.')
@@ -13,27 +16,66 @@ except mysql.connector.Error as err:
         print('Database does not exists.')
     else:
         print(err)
-
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 """Parsing csv file to dict(list()) structure."""
-csv_dict = dict() 
-csv_dict['Data'] = [] 
+# Dictionary with all the csv table.
+csv_dict = dict()
+# Fill the header of the table.
+csv_dict['Date'] = [] 
 csv_dict['Info'] = [] 
 csv_dict['Sum'] = [] 
 with open('payments.csv') as f: 
     csv_reader = csv.DictReader(f, delimiter=',') 
-    header = true
+    header = True
     for row in csv_reader: 
-        if index(row) == 0:
-            header = false
-            csv_dict.fromkeys(row) 
+        # KOSTIL'!!!!!!!!
+        if header:
+            header = False
         else: 
-            csv_dict['Data'].append(row['Data']) 
+            csv_dict['Date'].append(row['Date']) 
             csv_dict['Info'].append(row['Info']) 
             csv_dict['Sum'].append(row['Sum']) 
 
 
+"""Collecting all INNs from DB."""
+SQL_INN = 'SELECT inn FROM users'
+cursor.execute(SQL_INN)
+# Raw INNs from database (becomes in a list(tuple()) structure).
+db_raw_inns = cursor.fetchall()
+# Normalize structure (to list).
+db_clean_inns = []
+for i in range(len(db_raw_inns)):
+    db_clean_inns.append(db_raw_inns[i][0])
 
 
+"""Collecting all INNs from csv file."""
+# INNs from csv file (becomes a full 'Info' string).
+csv_raw_inns = []
+for raw_str in csv_dict['Info']:
+    csv_raw_inns.append(raw_str)
+
+
+"""Bring it to normal."""
+# Find db_clean_inns in csv_raw_inns (now list(Match object)).
+csv_almost_clean_inns = [] 
+for i in range(len(csv_raw_inns)): 
+    for j in range(len(db_clean_inns)): 
+        if re.search(db_clean_inns[j], csv_raw_inns[i]) != None: 
+            csv_almost_clean_inns.append(re.search(db_clean_inns[j], csv_raw_inns[i]))
+
+# Normalized csv_clean_inns (became to list()).
+csv_clean_inns = []
+for i in csv_almost_clean_inns:
+    csv_clean_inns.append(i.group(0))
+
+
+"""Compare two lists to get interceptions."""
+# Dict with INNs as keys and booleans as values. If INN in both lists -> True.
+result = dict.fromkeys(db_clean_inns)
+for i in csv_clean_inns:
+    for j in db_clean_inns:
+        if j in i:
+            result[j] = True
+            
 conn.close()
-
